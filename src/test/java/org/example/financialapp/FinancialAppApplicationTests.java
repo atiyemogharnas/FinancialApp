37f069d8ceb875cbdbce900060c52e6ff12d9337
atiye <atiyemogharnas@gmail.com>
2024-10-24 12:35:47 +0330

    create-project

---
+++ src/test/java/org/example/financialapp/FinancialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.financialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
